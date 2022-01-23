d78b9971700396cc14e0168a9db489db3b37e447
Andreas Kaempf <andreas.kaempf@soundience.com>
2022-01-23 11:18:06 +0100

    Added Part 2 of Day 12

--- README.md
+++ README.md
@@ -4,23 +4,29 @@ My solutions for the Advent of Code 2021 (done mostly in 2022)
 
 * **Day 1** (Python): Read through a list of numbers, and find two
   (then three) that add up to 2020 (*easy*)
+
 * **Day 2** (Python): Read each line, and validate password against 
   character-count rules (*easy*)
+
 * **Day 3** (Python): Count up the number of '#' encountered when traversing
   an array with a given dx/dy slope for each step. The rows wrap if x 
   coordinate is wider than the data (*easy*).
+
 * **Day 4** (Python): Simple validation of semi-structured text "passports",
   first looking for missing fields, then checking field contents against some
   simple rules (*easy*).
+
 * **Day 5** (Python): Apply a sequence of binary forward/back operators to find
   row and seat in an airplane, and compute a code for these. For Part 1, find
   the maximum seat ID, for Part 2 the missing one.
+
 * **Day 6** (Python): Read a file containing a sequence of letters on each row,
   each letter being a customer's "yes" response to a survey question. Customers
   are in groups, separated by newlines.  Part 1: For each group, count the
   number of questions to which anyone answered "yes". What is the sum of those
   counts?  Part 2: For each group, count the number of questions to which
   everyone answered "yes". What is the sum of those counts? (*easy*)
+
 * **Day 7** (Python): Recursively navigate a tree upwards (Part 1) and
   downwards (Part 2), to count how many bags contain a given type of bag, and
   how many bags a given type of bag ultimately contains. (*easy*)
@@ -45,6 +51,11 @@ My solutions for the Advent of Code 2021 (done mostly in 2022)
 * **Day 11** (Go): Change the state of a "seating plan" depending on 
   available seats immediately adjacent to every seat (Part 1), or visible 
   in any direction (Part 2). *Easy*, but took a long time.
+
+* **Day 12** (Go): Simulate movement of a "ship" based on simple 
+  instructions, directly for Part 1, relative to a "waypoint" for Part 2
+  (should have been easy, but instructions were hard to understand, 
+  so *medium*).
 
 To compile and run a Go program
 * Change into the directory with the program
--- day12/day12.go
+++ day12/day12.go
@@ -1,8 +1,9 @@
 // Advent of Code 2020, Day 12
 //
-// Simulate movement of a "ship" based on simple instructions.
+// Simulate movement of a "ship" based on simple instructions, directly
+// for Part 1, relative to a "waypoint" for Part 2.
 //
-// AK, 14/01/2022
+// AK, 14/01/2022 and 23/01/2022
 
 package main
 
@@ -19,8 +20,9 @@ func main() {
 	t, _ := ioutil.ReadFile("input.txt")
 	lines := strings.Split(string(t), "\n")
 
-	// Do part 1
+	// Do parts 1 and 2
 	part1(lines)
+	part2(lines)
 }
 
 // Part 1: interpret instructions as simple movement of the ship N/E/S/W, or
@@ -85,6 +87,73 @@ func part1(lines []string) {
 
 	dist := abs(x) + abs(y)
 	fmt.Printf("Part 1: ending distance = %d\n", dist)
+}
+
+// Part 2: interpret instructions as movement of a waypoint,
+// except F, which is movement of the ship towards the waypoint
+// a number of times
+func part2(lines []string) {
+
+	// Initial position of the waypoint is 10 units east (right) and 1 unit
+	// north (up), relative to the ship. East positions are positive X,
+	// West positions are negative X, North positions are positive Y,
+	// and South positions are negative Y.
+	var wx int64 = 10 // Position of the waypoint (relative to ship)
+	var wy int64 = 1  // (starts at 10 East, 1 North)
+	var sx, sy int64  // Position of the ship (starts at 0,0)
+
+	// Process each instruction
+	// N/S/E/W : move the waypoint north/south/east/weset by given value,
+	//   does not move the ship
+	// L/R : rotate the waypoint around the ship left/right (counterclockwise
+	//   and clockwise) the given number of degrees; moves the waypoint,
+	//   but not the ship
+	// F : move ship forward to the waypoint a number of times equal to the
+	//   given value; each time, moves the ship the total distance between
+	//   the ship and the waypoint, but does not move the waypoint (since
+	//   it is always relative to the ship)
+	for _, l := range lines {
+
+		// Skip blank lines and parse instruction
+		if len(l) == 0 {
+			continue
+		}
+		inst := l[0]
+		n, _ := strconv.ParseInt(l[1:], 10, 32)
+
+		// Execute instructions
+		if inst == 'N' { // Move just the waypoint
+			wy += n
+		} else if inst == 'S' {
+			wy -= n
+		} else if inst == 'E' {
+			wx += n
+		} else if inst == 'W' {
+			wx -= n
+		} else if inst == 'R' { // Rotate waypoint right around ship
+			for i := n; i > 0; i -= 90 { // repeat for each 90 degrees
+				wx_ := wx
+				wx = wy
+				wy = -wx_
+			}
+		} else if inst == 'L' { // Rotate waypoint left around ship
+			for i := n; i > 0; i -= 90 { // repeat for each 90 degrees
+				wx_ := wx
+				wx = -wy
+				wy = wx_
+			}
+
+		} else if inst == 'F' { // Move ship forward toward waypoint n times
+			sx += n * wx
+			sy += n * wy
+		}
+
+		fmt.Printf("%c %3d -> waypoint %d,%d ship %d,%d\n", inst, n, wx, wy, sx, sy)
+	}
+
+	// Manhattan distance, should be 286 for sample
+	dist := abs(sx) + abs(sy)
+	fmt.Printf("Part 2: ending distance = %d\n", dist)
 }
 
 // Simple absolute number
